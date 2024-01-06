5609ba1987f3e28a34d3e78176e32f1d9155669e
hieu2k2rem <155809668+hieu2k2rem@users.noreply.github.com>
2024-01-06 16:01:14 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,19 +1,39 @@
 from shop import db
+from datetime import datetime
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(30), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(180), unique=False, nullable=False)
-    profile = db.Column(db.String(180), unique=False, nullable=False, default='profile.jpg')
+class Addproduct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Numeric(10,2), nullable=False)
+    discount = db.Column(db.Integer, default=0)
+    stock = db.Column(db.Integer, nullable=False)
+    colors = db.Column(db.Text, nullable=False)
+    desc = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
+    brand = db.relationship('Brand', backref=db.backref('brands', lazy=True))
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('categories', lazy=True))
+
+    image1 = db.Column(db.String(150), nullable=False, default='image.jpg')
+    image2 = db.Column(db.String(150), nullable=False, default='image.jpg')
+    image3 = db.Column(db.String(150), nullable=False, default='image.jpg')
+
 
     def __repr__(self):
-        return '<User %r' % self.username
+        return '<Addproduct %r' %self.name
 
 
+class Brand(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
 
 
 db.create_all()
